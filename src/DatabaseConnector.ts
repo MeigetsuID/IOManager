c49ad2541a3871dcbe6efaad6eac886a40615961
Kamioda <kamiodamasato@outlook.com>
2024-08-06 22:10:41 +0900

    namespaceを除去

--- src/DatabaseConnector.ts
+++ src/DatabaseConnector.ts
@@ -1,27 +1,25 @@
 import { PrismaClient } from '@prisma/client';
 
-export namespace IOManager {
-    export class DatabaseConnector {
-        private static instance: PrismaClient;
-        private static connection: number = 0;
-        /**
-         * Represents the constructor of the Database class.
-         * Initializes a new instance of the Database class and increments the connection count.
-         */
-        constructor() {
-            if (DatabaseConnector.connection === 0) DatabaseConnector.instance = new PrismaClient();
-            DatabaseConnector.connection++;
-        }
-        [Symbol.dispose](): void {
-            DatabaseConnector.connection--;
-            if (DatabaseConnector.connection === 0) DatabaseConnector.instance.$disconnect();
-        }
-        /**
-         * Returns the PrismaClient instance used for database operations.
-         * @returns The PrismaClient instance.
-         */
-        protected get DB(): PrismaClient {
-            return DatabaseConnector.instance;
-        }
+export class DatabaseConnector {
+    private static instance: PrismaClient;
+    private static connection: number = 0;
+    /**
+     * Represents the constructor of the Database class.
+     * Initializes a new instance of the Database class and increments the connection count.
+     */
+    constructor() {
+        if (DatabaseConnector.connection === 0) DatabaseConnector.instance = new PrismaClient();
+        DatabaseConnector.connection++;
+    }
+    [Symbol.dispose](): void {
+        DatabaseConnector.connection--;
+        if (DatabaseConnector.connection === 0) DatabaseConnector.instance.$disconnect();
+    }
+    /**
+     * Returns the PrismaClient instance used for database operations.
+     * @returns The PrismaClient instance.
+     */
+    protected get DB(): PrismaClient {
+        return DatabaseConnector.instance;
     }
 }
