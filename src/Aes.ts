513847ab250e733595c75ab5db5e087ec2e64670
Kamioda <kamiodamasato@outlook.com>
2024-08-11 17:38:20 +0900

    メールアドレスの暗号化クラスの追加( #3 )

---
+++ src/Aes.ts
@@ -0,0 +1,41 @@
+import { createCipheriv, createDecipheriv, randomBytes } from 'node:crypto';
+import { existsSync, readFileSync, writeFileSync } from 'node:fs';
+
+export default class Aes {
+    private static readonly algorithm = 'aes-256-gcm';
+    private static readInitConfig() {
+        const initInfo = JSON.parse(readFileSync('./system/account/aes.dat', 'utf-8'));
+        return {
+            key: Buffer.from(initInfo.key, 'hex'),
+            iv: Buffer.from(initInfo.iv, 'hex'),
+        };
+    }
+    private static createKey(keyPath: string) {
+        const key = randomBytes(32);
+        const iv = randomBytes(12);
+        writeFileSync(keyPath, JSON.stringify({ key: key.toString('hex'), iv: iv.toString('hex') }));
+        return {
+            key: key,
+            iv: iv,
+        };
+    }
+    constructor(private keyPath: string) {
+        if (!existsSync(keyPath)) Aes.createKey(keyPath);
+    }
+    encrypt(text: string) {
+        /* v8 ignore next */
+        if (!existsSync(this.keyPath)) throw new Error('No encryption key found');
+        const { key, iv } = Aes.readInitConfig();
+        const cipher = createCipheriv(Aes.algorithm, key, iv);
+        const encrypted = Buffer.concat([cipher.update(text, 'utf-8'), cipher.final()]);
+        return encrypted.toString('hex');
+    }
+    decrypt(encryptedText: string) {
+        /* v8 ignore next */
+        if (!existsSync(this.keyPath)) throw new Error('No encryption key found');
+        const { key, iv } = Aes.readInitConfig();
+        const decipher = createDecipheriv(Aes.algorithm, key, iv);
+        const decrypted = Buffer.concat([decipher.update(Buffer.from(encryptedText, 'hex')), decipher.final()]);
+        return decrypted.toString('utf-8');
+    }
+};
---
+++ src/MailAddressEncryption.ts
@@ -0,0 +1,71 @@
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'node:fs';
+import Aes from './Aes';
+
+export default class MailAddressEncryption {
+    private encTextTable: string[];
+    private txtEncode: TextEncoder;
+    private txtDecode: TextDecoder;
+    private static createTextTable(): string[] {
+        const textTable = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'];
+        const shuffleArray = (array: string[]) => {
+            const cloneArray = [...array];
+
+            for (let i = cloneArray.length - 1; i >= 0; i--) {
+                const rand = Math.floor(Math.random() * (i + 1));
+                // 配列の要素の順番を入れ替える
+                const tmpStorage = cloneArray[i];
+                cloneArray[i] = cloneArray[rand];
+                cloneArray[rand] = tmpStorage;
+            }
+            return cloneArray;
+        };
+        const useText = shuffleArray(textTable).slice(0, 16);
+        const table: string[] = [];
+        for (let i = 0; i < 16; i++) {
+            for (let j = 0; j < 16; j++) table.push(useText[i] + useText[j]);
+        }
+        if (!existsSync('./system/account')) mkdirSync('./system/account', { recursive: true });
+        writeFileSync('./system/account/emailenc.csv', table.join(','));
+        return table;
+    }
+    constructor(private AESMgr: Aes) {
+        this.encTextTable = existsSync('./system/account/emailenc.csv')
+            ? readFileSync('./system/account/emailenc.csv', 'utf-8').split(',')
+            : MailAddressEncryption.createTextTable();
+        this.txtEncode = new TextEncoder();
+        this.txtDecode = new TextDecoder();
+    }
+    private toByte(text: string): number[] {
+        return Array.from(this.txtEncode.encode(text));
+    }
+    public encrypt(email: string) {
+        const positionRecord = {
+            len: email.length,
+        };
+        [...email].forEach((char, index) => {
+            if (!positionRecord[char]) positionRecord[char] = [];
+            positionRecord[char].push(index);
+        });
+        const jsonText = JSON.stringify(positionRecord);
+        return this.AESMgr.encrypt(
+            this.toByte(jsonText)
+                .map((byteVal: number) => this.encTextTable[byteVal])
+                .join('')
+        );
+    }
+    public decrypt(encryptedEmail: string) {
+        const textArr = this.AESMgr.decrypt(encryptedEmail).match(/.{2}/g);
+        if (textArr === null) throw new Error('Invalid encrypted email');
+        const byteNumArr = textArr.map(text => this.encTextTable.indexOf(text));
+        if (byteNumArr.includes(-1)) throw new Error('Invalid encrypted email');
+        const byteArr = new Uint8Array(byteNumArr);
+        const json = JSON.parse(this.txtDecode.decode(byteArr));
+        const MailAddressTextArr = new Array(json.len);
+        Object.keys(json).forEach(char => {
+            json[char].forEach(index => {
+                MailAddressTextArr[index] = char;
+            });
+        });
+        return MailAddressTextArr.join('');
+    }
+}
