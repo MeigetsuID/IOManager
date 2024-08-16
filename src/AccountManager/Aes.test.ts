ef179db46a1e509b74d0c79faf4faa9937b050e9
Kamioda <kamiodamasato@outlook.com>
2024-08-17 08:33:25 +0900

    引数に指定していたファイルパスを除去
    デフォルト引数の値と同じなため不要

--- src/AccountManager/Aes.test.ts
+++ src/AccountManager/Aes.test.ts
@@ -8,13 +8,13 @@ test('AES Key Auto Creation', () => {
 });
 
 test('AES Encryption', () => {
-    const aes = new Aes('./system/account/aes.dat');
+    const aes = new Aes();
     const text = 'test';
     expect(aes.encrypt(text)).toBe('a3ef13ef24baddd815802f87c1e59142');
 });
 
 test('AES Decryption', () => {
-    const aes = new Aes('./system/account/aes.dat');
+    const aes = new Aes();
     const text = 'a3ef13ef24baddd815802f87c1e59142';
     expect(aes.decrypt(text)).toBe('test');
 });
