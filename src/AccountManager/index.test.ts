ca5ff261e1e2bdca313d1067f1578c80b8e285d1
Kamioda <kamiodamasato@outlook.com>
2024-08-19 22:03:12 +0900

    データが見つからないケースのテストを追加( #3 )

--- src/AccountManager/index.test.ts
+++ src/AccountManager/index.test.ts
@@ -1,7 +1,7 @@
 import AccountManager from ".";
 const Account = new AccountManager();
 
-test('Get Account/System ID', async () => {
+test('Get Account/System ID/Found', async () => {
     const AccountInfo = await Account.SGetAccount('4010404006753');
     const Expect = {
         id: '4010404006753',
@@ -13,7 +13,12 @@ test('Get Account/System ID', async () => {
     expect(AccountInfo).toStrictEqual(Expect);
 });
 
-test('Get Account/User ID', async () => {
+test('Get Account/System ID/Not Found', async () => {
+    const AccountInfo = await Account.SGetAccount('1000011000005');
+    expect(AccountInfo).toBe(null);
+});
+
+test('Get Account/User ID/Found', async () => {
     const AccountInfo = await Account.GetAccount('meigetsu2020');
     const Expect = {
         user_id: 'meigetsu2020',
@@ -23,19 +28,40 @@ test('Get Account/User ID', async () => {
     expect(AccountInfo).toStrictEqual(Expect);
 });
 
-test('Get Account/Sign In/System ID', async () => {
+test('Get Account/User ID/Not Found', async () => {
+    const AccountInfo = await Account.GetAccount('meigetsu2021');
+    expect(AccountInfo).toBe(null);
+});
+
+test('Get Account/Sign In/System ID/OK', async () => {
     const AccountInfo = await Account.SignIn('4010404006753', 'password01');
     expect(AccountInfo).toBe('4010404006753');
 });
 
-test('Get Account/Sign In/User ID', async () => {
+test('Get Account/Sign In/User ID/OK', async () => {
     const AccountInfo = await Account.SignIn('meigetsu2020', 'password01');
     expect(AccountInfo).toBe('4010404006753');
 });
 
 
-test('Get Account/Sign In/Mail Address', async () => {
+test('Get Account/Sign In/Mail Address/OK', async () => {
     const AccountInfo = await Account.SignIn('info@mail.meigetsu.jp', 'password01');
     expect(AccountInfo).toBe('4010404006753');
 });
 
+
+test('Get Account/Sign In/System ID/Not Found', async () => {
+    const AccountInfo = await Account.SignIn('1000011000005', 'password01');
+    expect(AccountInfo).toBe(null);
+});
+
+test('Get Account/Sign In/User ID/Not Found', async () => {
+    const AccountInfo = await Account.SignIn('meigetsu2021', 'password01');
+    expect(AccountInfo).toBe(null);
+});
+
+
+test('Get Account/Sign In/Mail Address/Not Found', async () => {
+    const AccountInfo = await Account.SignIn('info1@mail.meigetsu.jp', 'password01');
+    expect(AccountInfo).toBe(null);
+});
