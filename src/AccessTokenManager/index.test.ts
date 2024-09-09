07f6b3aabc4e8bf68ed9156c40311706da31f09c
Kamioda <kamiodamasato@outlook.com>
2024-09-09 14:12:07 +0900

    fake timerを使うものと使わないものでdescribe分割( #6 )

--- src/AccessTokenManager/index.test.ts
+++ src/AccessTokenManager/index.test.ts
@@ -14,33 +14,37 @@ describe('Access Token Manager Sub Module Test', () => {
 describe('Access Token Manager Test', () => {
     const AccessToken = new AccessTokenManager('supervisor');
     const VirtualID = new VirtualIDManager();
-    beforeAll(() => {
-        jest.useFakeTimers({ now: FakeTime.getTime() });
-    });
-    afterAll(() => {
-        jest.useRealTimers();
-    });
-    test('Create Access Token', async () => {
-        const VID = await VirtualID.GetVirtualID(CreateAppID(), SystemID);
-        const TokenInfo = await AccessToken.CreateAccessToken(VID, ['supervisor']);
-        expect(TokenInfo).toStrictEqual({
-            token: expect.stringMatching(/^[0-9a-zA-Z]{256}$/),
-            expires_at: new Date(FakeTime.getTime() + 10800000),
+    describe('Use Time Mock', () => {
+        beforeAll(() => {
+            jest.useFakeTimers({ now: FakeTime.getTime() });
+        });
+        afterAll(() => {
+            jest.useRealTimers();
+        });
+        test('Create Access Token', async () => {
+            const VID = await VirtualID.GetVirtualID(CreateAppID(), SystemID);
+            const TokenInfo = await AccessToken.CreateAccessToken(VID, ['supervisor']);
+            expect(TokenInfo).toStrictEqual({
+                token: expect.stringMatching(/^[0-9a-zA-Z]{256}$/),
+                expires_at: new Date(FakeTime.getTime() + 10800000),
+            });
         });
     });
 
-    test('Check Access Token/Suervisor Cover Check/Return Virtual ID', async () => {
-        const VID = await VirtualID.GetVirtualID(CreateAppID(), SystemID);
-        const TokenInfo = await AccessToken.CreateAccessToken(VID, ['supervisor']);
-        const TokenText = TokenInfo.token;
-        const Check = await AccessToken.Check(TokenText, ['user.read', 'user.write']);
-        expect(Check).toBe(VID);
-    });
-
-    test('Check Access Token/Suervisor Cover Check/Return System ID', async () => {
-        const VID = await VirtualID.GetVirtualID(CreateAppID(), SystemID);
-        const TokenInfo = await AccessToken.CreateAccessToken(VID, ['supervisor']);
-        const Check = await AccessToken.Check(TokenInfo.token, ['user.read', 'user.write'], true);
-        expect(Check).toBe(SystemID);
+    describe('No Time Mock', () => {
+        test('Check Access Token/Suervisor Cover Check/Return Virtual ID', async () => {
+            const VID = await VirtualID.GetVirtualID(CreateAppID(), SystemID);
+            const TokenInfo = await AccessToken.CreateAccessToken(VID, ['supervisor']);
+            const TokenText = TokenInfo.token;
+            const Check = await AccessToken.Check(TokenText, ['user.read', 'user.write']);
+            expect(Check).toBe(VID);
+        });
+    
+        test('Check Access Token/Suervisor Cover Check/Return System ID', async () => {
+            const VID = await VirtualID.GetVirtualID(CreateAppID(), SystemID);
+            const TokenInfo = await AccessToken.CreateAccessToken(VID, ['supervisor']);
+            const Check = await AccessToken.Check(TokenInfo.token, ['user.read', 'user.write'], true);
+            expect(Check).toBe(SystemID);
+        });
     });
 });
