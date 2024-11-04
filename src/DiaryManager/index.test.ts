ef6200338316e3f086033bdc0f1d66bb1803fd12
Kamioda <kamiodamasato@outlook.com>
2024-11-04 17:12:51 +0900

    投稿者IDから日記一覧を取得するコードのテストを追加( #24 )

--- src/DiaryManager/index.test.ts
+++ src/DiaryManager/index.test.ts
@@ -1,4 +1,6 @@
+import CreateID from '@meigetsuid/idgenerator';
 import DiaryManager, { CreateDiaryID } from '.';
+import AccountManager from '../AccountManager';
 
 describe('Diary Manager Sub Module Test', () => {
     it('Create Diary ID', () => {
@@ -8,6 +10,7 @@ describe('Diary Manager Sub Module Test', () => {
 
 describe('Diary Manager All Test', () => {
     const Diary = new DiaryManager();
+    const Account = new AccountManager();
     describe('Create Diary', () => {
         it('Main Content', async () => {
             const Result = await Diary.CreateDiary('4010404006753', {
@@ -107,6 +110,79 @@ describe('Diary Manager All Test', () => {
                     },
                 ],
             });
+        });
+    });
+    describe('Get Diaries', () => {
+        const Cache = {
+            AccountID: '',
+            DiaryID: {
+                NoComment: '',
+                ContainComment: '',
+                CommentID: '',
+            },
+        };
+        beforeAll(async () => {
+            Cache.AccountID = await CreateID('diary_test01');
+            await Account.CreateAccount({
+                id: Cache.AccountID,
+                user_id: 'diary_test01',
+                name: 'TestUser',
+                mailaddress: 'get-diaries-test@mail.meigetsu.jp',
+                password: 'password01',
+                account_type: 4,
+            });
+            await Diary.CreateDiary(Cache.AccountID, {
+                title: 'Test Diary',
+                scope_of_disclosure: 0,
+                allow_comment: true,
+                content: 'Test Content',
+            }).then(DiaryID => {
+                Cache.DiaryID.NoComment = DiaryID;
+            });
+            await Diary.CreateDiary(Cache.AccountID, {
+                title: 'Test Diary',
+                scope_of_disclosure: 0,
+                allow_comment: true,
+                content: 'Test Content',
+            }).then(DiaryID => {
+                Cache.DiaryID.ContainComment = DiaryID;
+            });
+            await Diary.CreateDiary(Cache.AccountID, {
+                title: 'Test Comment',
+                scope_of_disclosure: 0,
+                allow_comment: true,
+                content: 'Test Comment',
+                comment_target: Cache.DiaryID.ContainComment,
+            }).then(DiaryID => {
+                Cache.DiaryID.CommentID = DiaryID;
+            });
+        });
+        it('Exists', async () => {
+            const Result = await Diary.GetDiaries(Cache.AccountID);
+            expect(Result.sort()).toStrictEqual(
+                [
+                    {
+                        id: Cache.DiaryID.NoComment,
+                        title: 'Test Diary',
+                        scope_of_disclosure: 0,
+                        upload_date: expect.any(Date),
+                        last_update_date: expect.any(Date),
+                        comments: 0,
+                    },
+                    {
+                        id: Cache.DiaryID.ContainComment,
+                        title: 'Test Diary',
+                        scope_of_disclosure: 0,
+                        upload_date: expect.any(Date),
+                        last_update_date: expect.any(Date),
+                        comments: 1,
+                    },
+                ].sort()
+            );
+        });
+        it('Not Exists', async () => {
+            const Result = await Diary.GetDiaries('4010404006754');
+            expect(Result).toStrictEqual([]);
         });
     });
 });
